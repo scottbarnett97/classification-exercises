133c5f6d843c7d50feaceb78dfbb5088432ae86a
Scott Barnett <scottbarnett97@gmail.com>
2023-04-13 15:06:45 -0500

    added todays exercises

---
+++ acquire.py
@@ -0,0 +1,50 @@
+import os
+""" This function pulls in the Titanic dataframe"""
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM passengers', env.get_db_url('titanic_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+    
+
+    """ This Function pulls in ght Iris dataframe"""
+    def get_iris_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('select * from measurements join species using (species_id)', env.get_db_url('iris_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+    
+
+""" This Function pulls in ght Telco_churn dataframe"""
+    def get_telco_data():
+    filename = "telco.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('select * from customers join contract_types using (contract_type_id) join internet_service_types using (internet_service_type_id) join payment_types using (payment_type_id)', env.get_db_url('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df 

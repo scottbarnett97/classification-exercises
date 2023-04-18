5271741b43ac4ff1d7d3d42147a64a27970265c2
Scott Barnett <scottbarnett97@gmail.com>
2023-04-17 21:43:35 -0500

    updates?

--- prepare.py
+++ prepare.py
@@ -45,10 +45,11 @@ def prep_telco(df):
                                 ,'paperless_billing','churn','contract_type','internet_service_type','payment_type'. 
     '''
     df = df.drop_duplicates()
+    df.total_charges = df.total_charges.str.replace(' ', '0').astype(float)
     df = df.drop(columns=['payment_type_id', 'internet_service_type_id','contract_type_id'])    
     dummy_df = pd.get_dummies(df[['gender','partner','dependents','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','contract_type','internet_service_type','payment_type']], dummy_na=False, drop_first=[True, True])
     df = pd.concat([df, dummy_df], axis=1)
-    df = df.total_charges.replace(' ',0).astype(float)
+    
     return df
 
 def split_data(df,strat):
